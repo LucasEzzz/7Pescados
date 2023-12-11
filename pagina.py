f78b56dbdf7986a2280f543743ee266fcf12f390
Lucas Marinho <87102239+LucasEzzz@users.noreply.github.com>
2023-12-11 12:39:43 -0300

    Update pagina.py

--- pagina.py
+++ pagina.py
@@ -5,7 +5,7 @@ from PIL import Image
 #background
 #page_bg_img = '''<style>
 #[data-testid="stAppViewContainer"] {
-background-color:#FFFFFF}
+#background-color:#FFFFFF}
 </style>'''
 st.markdown(page_bg_img, unsafe_allow_html=True)
 colT1, colT2 = st.columns([1, 5])
