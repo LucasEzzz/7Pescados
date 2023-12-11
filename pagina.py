e5ec04d9db4081b759f03b7bf880779cfb0a6167
Lucas Marinho <87102239+LucasEzzz@users.noreply.github.com>
2023-12-11 12:29:48 -0300

    Update pagina.py

--- pagina.py
+++ pagina.py
@@ -3,10 +3,10 @@ import streamlit as st
 from PIL import Image
 
 #background
-page_bg_img = '''<style>
-[data-testid="stAppViewContainer"] {
-background-color:#FFFFFF}
-</style>'''
+#page_bg_img = '''<style>
+  [data-testid="stAppViewContainer"] {
+    background-color:#FFFFFF}
+      </style>'''
 st.markdown(page_bg_img, unsafe_allow_html=True)
 colT1, colT2 = st.columns([1, 5])
 with colT2:
