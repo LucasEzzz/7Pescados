6b3ad7e403c839b9a2ec83ce5156c54cb36f5d3b
Lucas Marinho <87102239+LucasEzzz@users.noreply.github.com>
2023-12-11 12:44:22 -0300

    Update pagina.py

--- pagina.py
+++ pagina.py
@@ -8,7 +8,7 @@ page_bg_img = '''<style>
 background-color:#FFFFFF}
 </style>'''
 st.markdown(page_bg_img, unsafe_allow_html=True)
-colT1, colT2 = st.columns([5, 5])
+colT1, colT2 = st.columns([1, 1])
 with colT2:
   image = Image.open('project.png')
   st.image(image, width=400)
