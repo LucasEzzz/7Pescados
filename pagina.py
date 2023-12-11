6f1db2d224df5a0e4b214f60edee8d812405f7c8
Lucas Marinho <87102239+LucasEzzz@users.noreply.github.com>
2023-12-11 16:38:03 -0300

    Update pagina.py

--- pagina.py
+++ pagina.py
@@ -1,31 +1,14 @@
-#import
+import
 import streamlit as st
-#from PIL import Image
+from PIL import Image
 
-#background
-#page_bg_img = '''<style>
-#[data-testid="stAppViewContainer"] {
-#background-color:#2B5DB9}
-#</style>'''
-#st.markdown(page_bg_img, unsafe_allow_html=True)
-#colT1, colT2 = st.columns([5,5])
-#with colT2:
-  #image = Image.open('project2.jpeg')
-  #st.image(image, width=3000)
-
-page_bg_img = """
+background
+page_bg_img = '''<style>
 [data-testid="stAppViewContainer"] {
-background-image: Image.open('project2.jpeg')
-background-size: cover;
-}
-[data-testid="stHeader"]{
-background-color: rgba(0, 0, 0, 0);
-}
-[data-testid="stToolbar"]{
-right: 2rem;
-}
-</style>
-"""
-
+background-color:#2B5DB9}
+</style>'''
 st.markdown(page_bg_img, unsafe_allow_html=True)
-st.title("It's summer!")
+colT1, colT2 = st.columns([5,5])
+with colT2:
+  image = Image.open('project2.jpeg')
+  st.image(image, width=3000)
